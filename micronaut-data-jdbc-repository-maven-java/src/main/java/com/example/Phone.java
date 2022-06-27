a2c4551f228d2d11b6f5e0637f2b91cc3a4354a1
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-06-26 22:37:59 -0300

    Add @JsonIgnore to getPerson in Phone to avoid loop

--- micronaut-data-jdbc-repository-maven-java/src/main/java/com/example/Phone.java
+++ micronaut-data-jdbc-repository-maven-java/src/main/java/com/example/Phone.java
@@ -1,9 +1,11 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.tivrfoa.mapresultset.api.Table;
 
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.Relation;
 
 @Table
 @MappedEntity (value = "Phone")
@@ -12,6 +14,7 @@ public class Phone {
 	private int id;
 	private int number;
 
+	@Relation (value = Relation.Kind.MANY_TO_ONE)
 	private Person person;
 	
 	public int getId() {
@@ -27,12 +30,14 @@ public class Phone {
 		this.number = number;
 	}
 
+	@JsonIgnore
 	public Person getPerson() {
 		return person;
 	}
 	public void setPerson(Person person) {
 		this.person = person;
 	}
+
 	@Override
 	public String toString() {
 		return "Phone [id=" + id + ", number=" + number + "]";
